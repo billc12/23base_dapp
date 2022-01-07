3d3d4b6bf51da4753303aca2352d09a01e0b6336
iv172 <iv172@hotmail.com>
2022-01-07 13:32:06 +0800

    add utils

--- src/components/Select/PlainSelect.tsx
+++ src/components/Select/PlainSelect.tsx
@@ -1,94 +1,113 @@
-import React from 'react'
-import { Select as MuiSelect, styled, InputBase } from '@mui/material'
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
+import React, { useState } from 'react'
+import { Button, Fade, Menu } from '@mui/material'
 
 interface Props {
   children?: React.ReactNode
   placeholder: string
+  autoFocus?: boolean
+  width?: string
+  style?: React.CSSProperties
 }
 
-const StyledSelect = styled(MuiSelect)(({ theme }) => ({
-  backgroundColor: 'transparent',
-  cursor: 'pointer',
-  width: '72px',
-  position: 'relative',
-  border: 'none',
-  '& .MuiSelect-icon': {
-    color: theme.palette.primary.contrastText,
-    right: '16px',
-    fontSize: '16px'
-  },
-  // '&.Mui-focused': {
-  //   backgroundColor: 'transparent'
-  // },
-  '&:hover': {
-    color: 'rgba(255, 255, 255, 1)',
-    '& .MuiOutlinedInput-notchedOutline': {
-      border: 'none'
-    }
-  }
-}))
-
 export default function Select(props: Props) {
-  const { children, placeholder } = props
+  const { children, placeholder, autoFocus, style } = props
+
+  const [anchorEl, setAnchorEl] = useState<any>(null)
+  const open = Boolean(anchorEl)
+
+  const handleClick = (event: any) => {
+    event.currentTarget && setAnchorEl(event.currentTarget)
+  }
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
 
   return (
-    <StyledSelect
-      sx={{
-        '&:before': {
-          position: 'absolute',
-          zIndex: -1,
-          content: '"' + placeholder ? `'${placeholder}'` : '' + '"',
-          fontSize: 14,
-          fontWeight: 400
-        }
-      }}
-      autoWidth
-      placeholder={placeholder}
-      MenuProps={{
-        sx: {
+    <>
+      <Button
+        component="a"
+        type="button"
+        disableRipple
+        id="fade-button"
+        aria-controls={open ? 'fade-menu' : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? 'true' : undefined}
+        onClick={handleClick}
+        sx={{
+          height: '14px',
+          borderRadius: 0,
+          padding: 0,
+          color: theme => theme.palette.text.primary,
+          opacity: open ? 1 : 0.5,
+          '&:hover': {
+            color: theme => theme.palette.text.primary,
+            opacity: 1
+          },
+          '&.Mui-focused': {
+            color: theme => theme.palette.text.primary,
+            opacity: 1
+          },
+          ...style
+        }}
+      >
+        {placeholder}
+        <svg
+          width="10"
+          height="7"
+          viewBox="0 0 10 7"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          style={{ transform: open ? 'rotate(180deg)' : 'rotate(0)', stroke: '1px', marginLeft: 8 }}
+        >
+          <path d="M9.48535 1.24268L5.24271 5.48532L1.00007 1.24268" stroke="currentColor" />
+        </svg>
+      </Button>
+      <Menu
+        id="fade-menu"
+        MenuListProps={{
+          'aria-labelledby': 'fade-button'
+        }}
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        onClick={handleClose}
+        TransitionComponent={Fade}
+        sx={{
           '& .MuiPaper-root': {
-            width: 148,
+            width: 'max-content',
             borderRadius: '14px',
             marginTop: 6,
+            border: '1px solid rgba(0, 0, 0, 0.1)',
+            boxShadow: '0px 1px 8px rgba(0, 0, 0, 0.05)',
             backgroundColor: 'transparent',
             '& ul': {
-              background: '#0F0F10',
+              background: theme => theme.palette.background.paper,
               padding: '10px 20px 18px 20px'
             },
             '& li': {
               fontSize: 13,
               fontWeight: 400,
-              color: 'rgba(255, 255, 255, 0.4)',
+              color: theme => theme.palette.text.secondary,
               padding: '8px 0',
               border: 'none',
               outline: 'none',
               backgroundColor: 'transparent'
             },
             '& li:hover': {
-              color: theme => theme.palette.primary.main,
+              color: theme => theme.palette.text.primary,
               backgroundColor: 'transparent'
             },
             '& .Mui-selected': {
-              color: theme => theme.palette.primary.main,
+              color: theme => theme.palette.text.primary,
               backgroundColor: 'transparent'
             }
           }
-        },
-        anchorOrigin: {
-          vertical: 'bottom',
-          horizontal: 'left'
-        },
-        transformOrigin: {
-          vertical: 'top',
-          horizontal: 'left'
-        }
-      }}
-      IconComponent={ExpandMoreIcon}
-      input={<InputBase />}
-      renderValue={() => ''}
-    >
-      {children}
-    </StyledSelect>
+        }}
+        autoFocus={autoFocus}
+        placeholder={placeholder}
+      >
+        {children}
+      </Menu>
+    </>
   )
 }
--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -2,8 +2,10 @@ import React, { useCallback } from 'react'
 import { Tabs as MuiTabs, Tab, Box } from '@mui/material'
 
 interface Props {
-  titles: string[]
+  titles: string[] | JSX.Element[]
   contents: React.ReactNode[]
+  customCurrentTab?: number
+  customOnChange?: (val: number) => void
 }
 
 function TabPanel({ children, value, index }: { children: React.ReactNode; value: number; index: number }) {
@@ -11,23 +13,28 @@ function TabPanel({ children, value, index }: { children: React.ReactNode; value
 }
 
 export default function Tabs(props: Props) {
-  const { titles, contents } = props
+  const { titles, contents, customCurrentTab, customOnChange } = props
   const [value, setValue] = React.useState(0)
 
-  const onChange = useCallback((e: React.ChangeEvent<any>, value: any) => {
-    setValue(value)
-  }, [])
+  const onChange = useCallback(
+    (e: React.ChangeEvent<any>, value: any) => {
+      customOnChange ? customOnChange(value) : setValue(value)
+    },
+    [customOnChange]
+  )
 
   return (
     <Box sx={{ width: '100%' }}>
       <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-        <MuiTabs value={value} onChange={onChange}>
-          {titles.map(tab => (
+        <MuiTabs value={customCurrentTab !== undefined ? customCurrentTab : value} onChange={onChange} sx={{ mb: -1 }}>
+          {titles.map((tab, idx) => (
             <Tab
-              key={tab}
+              disableRipple
+              key={idx}
               label={tab}
               sx={{
-                fontWeight: 700,
+                padding: { xs: '20px 0px 17px 0px', md: '20px 10px 25px 0px' },
+                mr: { xs: 23, md: 25 },
                 textTransform: 'none',
                 color: theme => theme.palette.text.primary,
                 opacity: 0.4,
@@ -41,7 +48,7 @@ export default function Tabs(props: Props) {
         </MuiTabs>
       </Box>
       {contents.map((content, idx) => (
-        <TabPanel value={value} index={idx} key={idx}>
+        <TabPanel value={customCurrentTab !== undefined ? customCurrentTab : value} index={idx} key={idx}>
           {content}
         </TabPanel>
       ))}
